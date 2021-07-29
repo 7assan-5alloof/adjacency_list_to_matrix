6d7b75579a98ebcffb7045add0d4b198c31bcad2
7assan-5alloof <hassankhalloof20@gmail.com>
2021-07-29 14:34:08 +0400

    Incomplete Solution

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    freopen("a.in", "r", stdin);
+    int n;
+    cin >> n;
+    vector<vector<int>> outgoing_edges;
+    for (int i = 0; i < n; i < n; i++)
+    {
+
+    }
+
+    return 0;
+}
